09ee23270cff918fa9db29a418a23fb9b9a7f30f
Henry Chen <henchen@seas.upenn.edu>
2023-02-10 17:37:20 -0500

    fix error

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import GameUI from "./game-ui";
 
 async function init() {
   const gameui = new GameUI(new Game());
-  gameui.test();
   gameui.displayPlayerInfo();
   document.body.appendChild(gameui.getUI());
 }
