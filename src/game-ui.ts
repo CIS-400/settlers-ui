91e25664ee58e234d6ed3fb76a1801f389f60839
Aydan Gooneratne <aydangooneratne@Aydans-MacBook-Pro.local>
2023-02-10 16:30:12 -0500

    node positions

--- src/game-ui.ts
+++ src/game-ui.ts
@@ -15,15 +15,53 @@ class GameUI {
       backgroundColor: "#78bac2",
     });
     this.app.stage.interactive = true;
+    this.nodeSprites = [];
     this.handleNodeClick = this.handleNodeClick.bind(this);
 
+    this.initialize();
+  }
+
+  async initialize() {
+    const HEX_CROSS = 5; // hexes across middle row
+    const w = 90; // width in user units
+    const x_offset = 0.5 * (100 - w);
+    const h = 90; // height in user units
+    const y_offset = 0.5 * (100 - h);
+    const root3 = Math.sqrt(3);
+    const s = w / (HEX_CROSS * root3); // side length
+    let y = 0.5 * h - 3.5 * s; // starting y so that nodes are vertically aligned
+    let x = 0.5 * w - 1.5 * root3 * s; // starting x so that nodes are horizontally aligned
+    const rowSize = [7, 9, 11, 11, 9, 7]; // nodes per row
+    const halfRowSize = Math.floor(rowSize.length / 2);
+    let col = 0;
+    let row = 0;
+    let rx = x;
+    let ry = y;
+    const texture = await PIXI.Assets.load(require("../assets/wood-tile.svg"));
+
     // initialize nodes
-    this.nodeSprites = [];
     for (let i = 0; i < SETTLERS.NUM_NODES; i++) {
-      const ns = new PIXI.Sprite();
+      const ns = new PIXI.Sprite(texture);
       ns.onclick = () => this.handleNodeClick(i);
       ns.anchor.set(0.5);
+      ns.position.set(8 * (x + x_offset), 8 * (y + y_offset));
       this.nodeSprites.push(ns);
+      col++;
+      if (col === rowSize[row]) {
+        ry += row === halfRowSize - 1 ? s : 1.5 * s;
+        y = ry;
+        if (row < halfRowSize - 1) {
+          rx -= 0.5 * root3 * s;
+        } else if (row > halfRowSize - 1) {
+          rx += 0.5 * root3 * s;
+        }
+        x = rx;
+        col = 0;
+        row++;
+      } else {
+        x += 0.5 * root3 * s;
+        y += 0.5 * s * (col % 2 === (row < halfRowSize ? 1 : 0) ? -1 : 1);
+      }
     }
     this.app.stage.addChild(...this.nodeSprites);
   }
--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,10 @@
 import * as PIXI from "pixi.js";
 import { Game } from "settlers";
 import GameUI from "./game-ui";
-const { Text, Sprite, Assets } = PIXI;
-const rgbColor = PIXI.utils.rgb2hex;
-
-const g = new Game();
-const children: any = [];
-const appBg = rgbColor([100, 100, 100]);
-const app = new PIXI.Application({
-  width: 800,
-  height: 600,
-  backgroundColor: appBg,
-});
-const stage = app.stage;
-stage.interactive = true;
 
 async function init() {
   const gameui = new GameUI(new Game());
   document.body.appendChild(gameui.getUI());
-  console.log(g.toLog());
-  children.map((x: any) => app.stage.addChild(x));
-  // const viewElement = app.view;
-  // const evTgts = ["click"];
-  // evTgts.map(tgt => viewElement.addEventListener(tgt, events));
-  document.body.appendChild(app.view);
-  // load the texture we need
-  const texture = await Assets.load(require("../assets/wood-tile.svg"));
-
-  // This creates a texture from a 'bunny.png' image
-  const bunny = new Sprite(texture);
-
-  // Setup the position of the bunny
-  bunny.x = app.renderer.width / 2;
-  bunny.y = app.renderer.height / 2;
-
-  // Rotate around the center
-  bunny.anchor.x = 0.5;
-  bunny.anchor.y = 0.5;
-
-  // Add the bunny to the scene we are building
-  app.stage.addChild(bunny);
-
-  // Listen for frame updates
-  app.ticker.add(() => {
-    // each frame we spin the bunny around a bit
-    bunny.rotation += 0.01;
-  });
 }
 
 export default init();
