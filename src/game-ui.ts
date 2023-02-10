3b9242a30da6536e2624ca5ef2c92e5f0328b24f
Aydan Gooneratne <aydangooneratne@Aydans-MacBook-Pro.local>
2023-02-10 16:05:35 -0500

    did some work on board

--- src/game-ui.ts
+++ src/game-ui.ts
@@ -1,17 +1,46 @@
 import { Game } from "settlers";
+import * as SETTLERS from "settlers";
 import * as PIXI from "pixi.js";
 
 class GameUI {
   private readonly game: Game;
   private readonly app: PIXI.Application<PIXI.ICanvas>;
+  private readonly nodeSprites: PIXI.Sprite[];
 
   constructor(game: Game) {
     this.game = game;
     this.app = new PIXI.Application({
       width: 800,
       height: 800,
+      backgroundColor: "#78bac2",
     });
     this.app.stage.interactive = true;
+    this.handleNodeClick = this.handleNodeClick.bind(this);
+
+    // initialize nodes
+    this.nodeSprites = [];
+    for (let i = 0; i < SETTLERS.NUM_NODES; i++) {
+      const ns = new PIXI.Sprite();
+      ns.onclick = () => this.handleNodeClick(i);
+      ns.anchor.set(0.5);
+      this.nodeSprites.push(ns);
+    }
+    this.app.stage.addChild(...this.nodeSprites);
+  }
+
+  render() {
+    // draw board
+    // draw overlay
+  }
+
+  renderBoard() {
+    // tiles
+    // nodes
+    // edges
+  }
+
+  handleNodeClick(node: number) {
+    console.log(`node ${node} clicked`);
   }
 
   getUI() {
